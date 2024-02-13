c3d9efeaf9dd2a58ee419a25fc3cb266505efc5e
A5025 <tsai5025tunghan@gmail.com>
2024-02-13 14:59:43 +0800

    建立專案

---
+++ src/test/java/YeaMiko/CreatorPlatform/CreatorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package YeaMiko.CreatorPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
